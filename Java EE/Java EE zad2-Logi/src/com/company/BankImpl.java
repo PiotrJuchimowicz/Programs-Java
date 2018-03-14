16c1793bb6c04674f2b7ea6e5a4afbb63cf57c03
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-03-14 18:19:32 +0100

    Initial commit

---
+++ Java EE/Java EE zad2-Logi/src/com/company/BankImpl.java
@@ -0,0 +1,129 @@
+package com.company;
+
+import java.util.LinkedList;
+//Numer czlowieka na liscie to rowniez jego id
+ public class BankImpl implements Bank
+{
+    private LinkedList<Account> list;
+    private static int number;//Zlicza klientów
+
+    public BankImpl()
+    {
+        list = new LinkedList<Account>();
+        number=0;
+    }
+    //Zwraca id konta dla podanych parametrów
+    public Integer findAccount(String name,String address)
+    {
+        for(int i=0;i<list.size();i++)
+        {
+             if((list.get(i).getName().equals(name))&&(list.get(i).getAddress().equals(address)))
+                 return  new Integer(list.get(i).getId());
+        }
+            return null;
+    }
+    //Zwraca konto o podanym id,jesli nie znajdzie zwaraca null
+    public Account findObject(int id)
+    {
+      for(int i=0;i<list.size();i++)
+      {
+          if(list.get(i).getId()==id)
+              return list.get(i);
+      }
+      return null;
+    }
+
+
+
+    public Integer createAccount(String name, String address)
+    {
+
+       Integer  id=findAccount(name,address);
+       if(id!=null)
+           return id;
+       else
+       {
+           list.add(new Account(name,address,0));
+           number++;
+
+           return  number-1;
+       }
+
+
+    }
+
+    //Dodaje srodki użytkownikowi o danym koncie
+     public void deposit(Integer id, long amount) throws AccountIdException
+     {
+
+         //Szuka konta o takim id i dodaje mu środki
+        for(int i=0;i<list.size();i++)
+            if (list.get(i).getId() == id)
+            {
+                list.get(i).setFunds(list.get(i).getFunds()+amount);
+                return;
+            }
+        //Wyrzuca wyjątek jeśli nie ma takiego konta
+            throw new  AccountIdException();
+     }
+
+   public  long getBalance(Integer id)
+    {
+        if(findObject(id)==null)
+            throw new AccountIdException();
+
+        return (long)findObject(id).getFunds();
+
+    }
+
+   public  void withdraw(Integer id, long amount) throws AccountIdException,InsufficientFundsException
+    {
+        if(findObject(id)==null)
+            throw new AccountIdException();
+
+        else
+        {
+            float sum=findObject(id).getFunds();
+            if(sum>=amount)
+                findObject(id).setFunds(sum-amount);
+            else
+                throw new InsufficientFundsException();
+        }
+    }
+
+     public void transfer(Integer idSource, Integer idDestination, long amount)
+     {
+         //Jesli podano zle id kont
+         if((findObject(idSource)==null) || (findObject(idDestination)==null))
+             throw new AccountIdException();
+         else
+         {
+             //Jesli na koncie z ktorego idzie przelew nia ma wystarczajacych srodkow
+             if(findObject(idSource).getFunds()<amount)
+                 throw new InsufficientFundsException();
+             else
+             {
+                 float a=findObject(idSource).getFunds();
+                 float b=findObject(idDestination).getFunds();
+                 a=a-amount;
+                 b=b+amount;
+                 findObject(idSource).setFunds(a);
+                 findObject(idDestination).setFunds(b);
+             }
+         }
+     }
+
+    public LinkedList<Account> getList()
+    {
+        return list;
+    }
+
+    public static int getNumber()
+    {
+        return number;
+    }
+
+
+
+
+}
---
+++ Java EE/Jave EE zad 3 - Maven/src/main/java/com/PiotrJuchimowicz/Bank.java
@@ -0,0 +1,71 @@
+package com.PiotrJuchimowicz;
+
+public interface Bank
+{
+
+
+    /**
+     * Tworzy nowe lub zwraca id istniejącego konta.
+     * @param name imie i nazwisko własciciela
+     * @param address adres własciciela
+     * @return id utworzonego lub istniejacego konta.
+     */
+    Integer createAccount(String name, String address);
+    /*
+
+     * Znajduje identyfikator konta.
+     * @param name imię i nazwisko właściciela
+     * @param address adres właściciela
+     * @return id konta lub null, gdy brak konta o podanych parametrach
+     */
+    Integer findAccount(String name, String address);
+
+    /*
+     * Dodaje srodki do konta.
+     * @param id
+     * @param amount srodki
+     * @throws AccountIdException, gdy id konta jest nieprawidlowe
+     */
+    void deposit(Integer id, long amount);
+
+    /*
+     * Zwraca ilosc srodkow na koncie.
+     * @param id
+     * @return srodki
+     * @throws AccountIdException, gdy id konta jest nieprawidlowe
+     */
+    long getBalance(Integer id);
+
+    /*
+     * Pobiera srodki z konta.
+     * @param id
+     * @param amount srodki
+     * @throws AccountIdException, gdy id konta jest nieprawidlowe
+     * @throws InsufficientFundsException, gdy srodki na koncie nie sa wystarczajace do wykonania operacji
+     */
+    void withdraw(Integer id, long amount);
+
+    /*
+     * Przelewa srodki miedzy kontami.
+     * @param idSource
+     * @param idDestination
+     * @param amount srodki
+     * @throws AccountIdException, gdy id konta jest nieprawidlowe
+     * @throws InsufficientFundsException, gdy srodki na koncie nie sa wystarczajace do wykonania operacji
+     */
+    void transfer(Integer idSource, Integer idDestination, long amount);
+
+    class InsufficientFundsException extends RuntimeException
+    {
+
+
+    };
+    class AccountIdException extends RuntimeException
+    {
+
+
+    };
+
+
+}
+
