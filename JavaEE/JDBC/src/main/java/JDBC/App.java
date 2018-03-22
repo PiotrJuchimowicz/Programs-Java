15718b5d403a82da6d77eb9f177555679bf403ad
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-03-22 14:43:27 +0100

    DB

--- JavaEE/JDBC/src/main/java/JDBC/App.java
+++ JavaEE/JDBC/src/main/java/JDBC/App.java
@@ -1,5 +1,20 @@
 package JDBC;
 
+/**
+ Commands in Oracle XE DB
+ CREATE TABLE Client(
+ id INTEGER PRIMARY KEY ,
+ firstName VARCHAR(50),
+ lastName VARCHAR (50),
+ pesel VARCHAR (11) UNIQUE NOT NULL ,
+ email VARCHAR (50) );
+
+ CREATE TABLE Account (
+ id INTEGER PRIMARY KEY ,
+ notes VARCHAR(255),
+ balance NUMBER(19),
+ id_client INTEGER CONSTRAINT fk_client REFERENCES Client(ID) );
+ */
 
 import java.sql.*;
 public class App 
