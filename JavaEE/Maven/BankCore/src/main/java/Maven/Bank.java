b6b4a6fc9e4b494dda81a0dc5ace34b846ad9ede
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-03-25 20:55:50 +0200

    maven update

--- JavaEE/Maven/BankCore/src/main/java/Maven/Bank.java
+++ JavaEE/Maven/BankCore/src/main/java/Maven/Bank.java
@@ -11,7 +11,7 @@ public interface Bank
      * @return id utworzonego lub istniejacego konta.
      */
     Integer createAccount(String name, String address);
-    /*
+    /**
 
      * Znajduje identyfikator konta.
      * @param name imię i nazwisko właściciela
@@ -20,7 +20,7 @@ public interface Bank
      */
     Integer findAccount(String name, String address);
 
-    /*
+    /**
      * Dodaje srodki do konta.
      * @param id
      * @param amount srodki
@@ -28,7 +28,7 @@ public interface Bank
      */
     void deposit(Integer id, long amount);
 
-    /*
+    /**
      * Zwraca ilosc srodkow na koncie.
      * @param id
      * @return srodki
@@ -36,7 +36,7 @@ public interface Bank
      */
     long getBalance(Integer id);
 
-    /*
+    /**
      * Pobiera srodki z konta.
      * @param id
      * @param amount srodki
@@ -45,7 +45,7 @@ public interface Bank
      */
     void withdraw(Integer id, long amount);
 
-    /*
+    /**
      * Przelewa srodki miedzy kontami.
      * @param idSource
      * @param idDestination
