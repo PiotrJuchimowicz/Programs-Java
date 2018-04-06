7240a53059add345f3cd7392808018f5db62b226
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-04-06 18:22:03 +0200

    learning writing and saving to file

--- SimplePrograms/FileOperations/src/com/company/Main.java
+++ SimplePrograms/FileOperations/src/com/company/Main.java
@@ -1,8 +1,63 @@
 package com.company;
 
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws  IOException
+    {
+        //for text files
+        String file="plik.txt";
+        FileWriter fileWriter=null;
+        Integer i = 10;
+
+          fileWriter = new FileWriter(file, true);
+          fileWriter.write("something");
+          fileWriter.write(i.toString());
+          fileWriter.close();
+
+        BufferedReader bufferedReader=null;
+        FileReader fileReader=null;
+        fileReader=new FileReader(file);
+        bufferedReader=new BufferedReader(fileReader);
+        String string=null;
+
+        int a;
+        char b;
+
+        while((a=bufferedReader.read())!=-1)
+        {
+            b= (char)a;
+            System.out.print(b + " ");
+        }
+
+        fileReader.close();
+        bufferedReader.close();
+
+        //for binary files
+
+        String fileName="file.txt";
+        FileOutputStream fileOutputStream=new FileOutputStream(fileName);
+        DataOutputStream dataSteam= new DataOutputStream(fileOutputStream);
+        dataSteam.writeInt(2);
+        dataSteam.writeFloat((float)2.42);
+
+        fileOutputStream.close();
+        dataSteam.close();
+
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+        int k = dataInputStream.readInt();
+        float h=dataInputStream.readFloat();
+
+        System.out.println("");
+        System.out.println(k + " " + h);
+
+
+
+
+
+
     }
 }
