09e4b29744ce06d4bce7a1ffbd10a896216f5671
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-05-10 14:42:54 +0200

    up

--- SimplePrograms/InnerClasses/src/InnerClasses/Main.java
+++ SimplePrograms/InnerClasses/src/InnerClasses/Main.java
@@ -1,11 +1,17 @@
 package InnerClasses;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args){
 
         OuterClass outerClass = new OuterClass();
 
         OuterClass.InnerClass innerClass = outerClass.new InnerClass();
+
+
 
 
 
