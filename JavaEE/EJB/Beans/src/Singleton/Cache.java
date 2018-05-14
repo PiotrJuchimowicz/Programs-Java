41255a274832a9bbb5045d4b4b4a5da32dc35742
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-05-14 18:46:11 +0200

    EJB

--- JavaEE/EJB/Beans/src/Singleton/Cache.java
+++ JavaEE/EJB/Beans/src/Singleton/Cache.java
@@ -1,21 +1,28 @@
 package Singleton;
 
+import CuntedString.CountedString;
+import Remote.CacheRemote;
+
 import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import java.util.HashMap;
 import java.util.Map;
 
 @Singleton
-public class Cache implements  CacheLocal,CacheRemote {
-     private Map<String,Object> cache;
+@Startup
+public class Cache implements CacheLocal, CacheRemote {
+    private static final Map<String, Object> cache = new HashMap<>();
 
-    public Cache()
-    {
-        cache = new HashMap<>();
+
+    public static Map<String, Object> getCache() {
+        return Cache.cache;
     }
 
 
     @Override
     public void add(String key, Object value) {
+        CountedString cs = (CountedString) value;
+        cs.startCount();
         cache.put(key, value);
 
     }
@@ -23,12 +30,24 @@ public class Cache implements  CacheLocal,CacheRemote {
     @Override
     public Object get(String key) {
 
+        Object o = cache.get(key);
+        CountedString cs = (CountedString)o;
+        cs.startCount();
+
         return cache.get(key);
     }
 
     @Override
     public boolean contains(String key) {
+        Object o = cache.get(key);
+        if(o!=null)
+        {
+            CountedString cs = (CountedString)o;
+            cs.startCount();
+        }
+
         return cache.containsKey(key);
+
     }
 
     @Override
