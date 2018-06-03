cd336568aaef2e56fc14f9f647fc280cad4dc44d
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-06-03 22:49:09 +0200

    m

---
+++ JavaEE/Logs/src/com/company/Account.java
@@ -0,0 +1,42 @@
+package com.company;
+
+//Każde konto  w chiwli utworzenia otrzymuje automatycznie unikalne id
+//Numeracja od id=0
+public class Account
+{
+    private int id;
+    private String name,address;
+    private float funds;
+
+    public Account(){};
+
+    public Account(String name,String addres,float funds)
+    {
+        this.id=BankImpl.getNumber();
+        this.name=name;
+        this.address=addres;
+        this.funds=funds;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public float getFunds() {
+        return funds;
+    }
+
+    public void setFunds(float funds)
+    {
+        this.funds = funds;
+    }
+}
